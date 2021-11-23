7af982e09aaf2ef8af0abf9d347db6a516fcd706
Filip Gałczyk <filip.galczyk@hxs.pl>
2021-11-23 21:31:16 +0100

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { GiRing } from "react-icons/gi";
+import { VscCode } from "react-icons/vsc";
+import { WiCloudy } from "react-icons/wi";
+
+const Navbar = () => {
+    return (
+        <div className="fixed top-0 left-0 px-4
+        w-screen h-16 
+        flex flex-row
+        bg-gray-900 text-white shadow-lg">
+            <NavbarIcon text="LOTR Theme" icon = {<GiRing size="48"/>}></NavbarIcon>
+            <span className="ml-auto"></span>
+            <NavbarIcon text="Checkout my Github" icon = {<VscCode size="48"/>}></NavbarIcon>
+            <NavbarIcon text="Weather API" icon = {<WiCloudy size="50"/>}></NavbarIcon>
+        </div>
+    )
+};
+
+const NavbarIcon = ({ icon, text = '' }) => (
+    <div className="navbar-icon group">
+        {icon}
+        <span className="navbar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+);
+
+export default Navbar;
