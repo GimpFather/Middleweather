815c60e1473df46921db34967d3e1f5418fcbbbb
Filip Gałczyk <filip.galczyk@hxs.pl>
2021-11-23 23:19:20 +0100

    Seting up Hero component and importing font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="flex">
       <Navbar></Navbar>
-
+      <Hero></Hero>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div className="
+        mt-16
+        w-screen h-screen
+        flex flex-row
+        bg-gray-900 text-white shadow-lg">
+            <p>Here Hero</p>
+        </div>
+    )
+};
+
+export default Hero;
